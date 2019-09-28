b3fb56174d158f332ce22bb395aa37c57aeec0a7
Henrique Arthur <hnrq.art@gmail.com>
2019-09-28 11:10:54 -0300

    Testing ad banner

--- lib/ui/main_screen/main_screen.dart
+++ lib/ui/main_screen/main_screen.dart
@@ -1,10 +1,54 @@
+import 'package:firebase_admob/firebase_admob.dart';
 import 'package:flutter/material.dart';
 import 'package:simulador_timbeta/ui/main_screen/components/pacote_month_box.dart';
 import 'package:simulador_timbeta/ui/main_screen/components/recarga_month_box.dart';
 import 'package:simulador_timbeta/ui/main_screen/result.dart';
-import 'package:simulador_timbeta/ui/shared_components/ad_banner.dart';
 
-class MainScreen extends StatelessWidget {
+class MainScreen extends StatefulWidget {
+  @override
+  _MainScreenState createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  static const admobAppId = 'ca-app-pub-6254443832700241~9515735085';
+  static const adUnitId = 'ca-app-pub-6254443832700241/7847430198';
+  static const MobileAdTargetingInfo adTargetingInfo = MobileAdTargetingInfo(
+    childDirected: false,
+  );
+
+  BannerAd _bannerAd;
+
+  BannerAd createBannerAd() {
+    return BannerAd(
+      adUnitId: adUnitId,
+      size: AdSize.banner,
+      targetingInfo: adTargetingInfo,
+      listener: (MobileAdEvent event) {
+        print("BannerAd event is $event");
+      },
+    );
+  }
+
+  @override
+  void initState() {
+    FirebaseAdMob.instance.initialize(appId: admobAppId);
+
+    _bannerAd = createBannerAd();
+    _bannerAd
+      ..load()
+      ..show(
+        anchorType: AnchorType.bottom,
+      );
+
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _bannerAd?.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -49,7 +93,6 @@ class MainScreen extends StatelessWidget {
         PacoteMonthBox(number: 3),
         const SizedBox(height: 32.0),
         Center(child: Result()),
-        AdmobBanner(),
       ],
     );
   }
